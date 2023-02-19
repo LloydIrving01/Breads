94c1885dd642e4569a9c778a2b6884f382c9a1d0
LloydIrving01 <brnrdvl709@gmail.com>
2023-02-19 10:39:37 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,6 +32,13 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// Edit
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
 
 // Show
 breads.get('/:arrayIndex', (req, res) => {
@@ -44,11 +51,21 @@ breads.get('/:arrayIndex', (req, res) => {
     }   
 })
 
-  
 // Delete
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// Update
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -8,13 +8,9 @@ function Show ({bread, index}) {
         <h2>Show Page</h2>
         <h3>{bread.name}</h3>
         <p>
-          and it
-          {
-            bread.hasGluten
-            ? <span> does </span>
-            : <span> does NOT </span>
-          }
-          have gluten.
+          and it {
+              bread.hasGluten ? <span> does </span> : <span> does NOT </span>
+          } have gluten.
         </p>
         <img src={bread.image} alt = {bread.name}/>
         <li><a href="/breads">Go home</a></li>
